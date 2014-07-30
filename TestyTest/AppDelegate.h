8c972a2d96afa74006135f042c84e85320b8b5ef
Ian Barber <ian.barber@gmail.com>
2014-07-30 10:11:15 +0100

    Initial Commit

---
+++ TestyTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestyTest
+//
+//  Created by Ian Barber on 30/07/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestyTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestyTest
+//
+//  Created by Ian Barber on 30/07/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
